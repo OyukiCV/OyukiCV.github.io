70424a01c18da4cd07eaf7dc8d1ed92994fc3e9b
OyukiCV <oyukivel@hotmail.com>
2020-02-20 23:05:28 -0600

    nueva version de portafolio en rama-oyuki2

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function menuHamburger() {
-    let itemsNav = document.getElementById('items-nav');
-    itemsNav.classList.toggle('items-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-    menuOpen.classList.toggle('menu-open');
+// function menuHamburger() {
+//     let itemsNav = document.getElementById('items-nav');
+//     itemsNav.classList.toggle('items-nav-open');
+//     let menuOpen = document.getElementById('menu-toggle');
+//     menuOpen.classList.toggle('menu-open');
 
-}
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
