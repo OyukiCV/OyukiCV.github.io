7a81f2bba76666ed1f1b31cca2f5d2a10fdf0610
OyukiCV <oyukivel@hotmail.com>
2020-02-23 22:25:30 -0600

    actualizo habilidades y footer portafolio

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// function menuHamburger() {
-//     let itemsNav = document.getElementById('items-nav');
-//     itemsNav.classList.toggle('items-nav-open');
-//     let menuOpen = document.getElementById('menu-toggle');
-//     menuOpen.classList.toggle('menu-open');
-
-// }
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
